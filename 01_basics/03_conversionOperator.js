0fbe7d6a4bca983f1a6d21d57ef2667a8bfcc2b5
nirmakar24 <166800034+karnirmalya@users.noreply.github.com>
2025-06-05 07:10:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,55 @@
+// let score="33abc"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// let score1=undefined
+// let score2=null
+// let score3="hitesh"
+// let score4=true
+
+// let valueInNumber1= Number(score1)
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1)
+
+// let valueInNumber2= Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+
+// let valueInNumber3= Number(score3)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
+
+// let valueInNumber4= Number(score4)
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)
+
+//"33" => 33
+//"33abc" => NaN(Not a Number)
+//true =>1
+//false=> 0
+//null => 0
+//undefined =>NaN
+
+let isLoggedIn=1
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(isLoggedIn)
+
+ // 0 => false
+// 1 => true
+// "" => false
+// "abc" => true
+// null => false
+// undefined => false
+// NaN => false
+
+let someNumber=54
+let stringNumber= String(someNumber)
+console.log( stringNumber);
+console.log(typeof stringNumber);
