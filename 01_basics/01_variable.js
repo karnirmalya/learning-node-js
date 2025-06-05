ca032d46c1742a31377adc519437100c870c77e7
nirmakar24 <166800034+karnirmalya@users.noreply.github.com>
2025-06-05 06:16:10 +0000

    datatype

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -2,9 +2,11 @@ const accountId=17528
 var accountPassword="23435361"
 let accountEmail="sdgfuywv@gmail.com"
 accountCity="Mumbai"
+let accountState
+
 console.table([accountId, accountPassword, accountEmail, accountCity]);
 accountEmail="nirmalya23@gmail.com"
 // accountId=34345
 accountPassword="23213"
 accountCity="Kolkata"
-console.table([accountId, accountPassword, accountEmail, accountCity]);
+console.table([accountId, accountPassword, accountEmail, accountCity,accountState]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"  //treat all js code as new version
+
+// alert("hello")  //we are using node js ,not browser
+
+console.log(3
+    +
+    3
+);      // bad code readability
+
+let name="Nirmalya"
+let age=22
+let isLoggedIn=false
+
+//DATA TYPES
+
+//PRIMITIVE DATA TYPES
+
+//number =>2^53
+//bigint
+//string =>""
+//boolean => true/false
+//null =>standalone value
+//undefined
+//symbol => uniqueness
+
+//NON-PRIMITIVE DATA TYPES
+
+//objects
+
+console.log(typeof("nirmalya"))
+console.log(typeof null)
+console.log(typeof undefined)
